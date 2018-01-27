5a4fa868695991c54a2cd2b6756ed41676f12e6d
MarcDAFrame <mframe45@gmail.com>
2018-01-27 11:37:19 -0500

    pushing the static and template file

--- web/api.py
+++ web/api.py
@@ -1,8 +1,5 @@
 from flask import Flask, g, render_template, make_response, redirect
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask.views import View
-from flask_login import LoginManager
-from models import db, User
 
 import random, string, time
 
@@ -10,42 +7,31 @@ import random, string, time
 #CONFIG AND INIT
 app = Flask(__name__)
 app.config['DEBUG'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #This adds signifcant overhead
+
+#SQL
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #This adds signifcant overhead
+# with app.app_context():
+#     db.init_app(app)
+
+
+
 app.secret_key = ''.join(random.choice(string.ascii_uppercase+string.digits) for x in range(32))
 app.config['SESSION_TYPE'] = 'filesystem'
-with app.app_context():
-    db.init_app(app)
 
 
 
-def init_db():
-    print("init db")
-    with app.app_context():
-        db.create_all()
-        new_user = User(username='loc', password=generate_password_hash('1'), email='email')#gcacadmin, gcacadminpassword123
-        db.session.add(new_user)
-        db.session.commit()
+# def init_db():
+#     print("init db")
+#     with app.app_context():
+#         db.create_all()
+#         db.session.commit()
 
-from views import Index, Login, Logout, Register, Admin, UserPage, SaveProfile
+from views import Index 
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def get_user(ident):
-    return User.query.get(int(ident))
-
-app.add_url_rule('/login', view_func=Login.as_view('login'))
-app.add_url_rule('/logout', view_func=Logout.as_view('logout'))
 # app.add_url_rule('/register', view_func=Register.as_view('register'))
-app.add_url_rule('/', view_func=Index.as_view('index'))
-app.add_url_rule('/admin', view_func=Admin.as_view('admin'))
 
-app.add_url_rule('/save', view_func=SaveProfile.as_view('Save Profile'))
-app.add_url_rule('/<username>', view_func=UserPage.as_view('User Page'))
+app.add_url_rule('/', view_func=Index.as_view('Index'))
 
 #API
 
--- web/views.py
+++ web/views.py
@@ -1,9 +1,6 @@
-from models import db, User
+from models import db
 from flask import Flask, render_template, make_response, redirect, Response, request, jsonify
 from flask.views import MethodView
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
-from werkzeug.security import generate_password_hash
-from werkzeug import secure_filename
 import time
 
 import os
@@ -11,159 +8,28 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # my_file = os.path.join(BASE_DIR, 'myfile.txt')
 
-class SaveProfile(MethodView):
-    @login_required
-    def get(self):
-        return make_response(render_template('edit.html'), 200, {'Content-Type': 'text/html'})
-    @login_required
-    def post(self):
-        # print(request.form)
-        # user = db.session.query(User).filter_by(username=current_user).first()
-        user = current_user
-
-        if request.form['facebook'] != "":
-            user.facebook = request.form['facebook']
-        if request.form['twitter'] != "":
-            user.twitter = request.form['twitter']
-        if request.form['instagram'] != "":
-            user.instagram = request.form['instagram']
-
-        db.session.commit()
-
-        return Response('success')
-        # return Response(request.args)
-
-class UserPage(MethodView):
-    def get(self, username):
-        user = db.session.query(User).filter_by(username=username).first()
-
-        # return Response(username+' user profile')
-        return make_response(render_template('userpage.html', facebook=user.facebook, twitter=user.twitter, instagram=user.instagram), 200, {'Content-Type': 'text/html'})
 
 class Index(MethodView):
-    def get(self, username):
+    def get(self):
         # return Response(username+' user profile')
-        return make_response(render_template('index.html'), 200, {'Content-Type': 'text/html'})
-
-class Register(MethodView):
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('register.html', errors=[]), 200, headers)
-
+        # return make_response(render_template('html.html'), 200, {'Content-Type': 'text/html'})
+        return render_template('index.html')
+    
     def post(self):
-        print('register post')
-        try:
-            username = request.form['username']
-            password = request.form['password']
-            email = request.form['email']
-        except Exception as e:
-            print(e)
-            pass
+        # print(request.form)
+        # print(request.args)
 
-        print("register", username, password, email)
+        img = request.files.get('picture', '')
 
-        headers = {'Content-Type': 'text/html'}
-        errors = []
-        success = True
-        if len(password) < 8:
-            # return ('password is too short (8 or more characters)', 412)
-            errors.append('password is too short (8 or more characters)')
-            success = False
-
-        if not username:
-            errors.append('no username provided')
-            success = False
-
-        if not password:
-            errors.append('no password provided')
-            success = False
-
-        if not email:
-            errors.append('no email provided')
-            success = False
-
-        if User.username_taken(username):
-            return ('username is not unique', 412)
-            errors.append('username is not unique')
-            success = False
-
-        try:
-            new_user = User(username=username, password=generate_password_hash(password), email=email)
-        except Exception as e:
-            # print(e)
-            # print('couldnt create new user')
-            errors.append('server error')
-            success = False
-
-        if success:
-            try:
-                db.session.add(new_user)
-            except:
-                print('couldnt session cant add')
-                return make_response(render_template('error.html', e=500), 200, headers)
-            try:
-                db.session.commit()
-                return ('successfully registered', 200)
-            except:
-                print('couldnt commit')
-                return make_response(render_template('error.html', e=500), 200, headers)
+        if img:
+            print('test')
         else:
-            return make_response(render_template('register.html', errors=errors), 200, headers)
+            return make_response('error: no image uploaded', '500', {'Content-Type': 'text'})
 
 
-class Login(MethodView):
-
-    def get_template_name(self):
-        raise NotImplementedError()
-
-    def post(self):
-        username = request.form['username']
-        password = request.form['password']
-        user = None
-
-        headers = {'Content-Type': 'text/html'}
-        errors = []
-
-        # print(username, password)
-        try:
-            user = db.session.query(User).filter_by(username=username).first()
-            print(user)
-        except:
-            # return Response("user name not recognized")
-            errors.append('no user named %s' %username)
-
-        if not username or not password:
-            errors.append('insure that username and password are not blank')
-
-        # if notL user:
-            # return Response('no user named %s'%username)
-            # errors.append('no user named %s'%username)
-        try:
-            if  user.verify_password(password, user.password) and username and password:
-                login_user(user)
-                return redirect(request.args.get("next"))
-                # return Response('logged in')
-            else:
-                errors.append('password or username is incorrect')
-        except:
-            errors.append('password or username is incorrect')
-
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('login.html', errors=errors), 200, headers)
-
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('login.html', errors=[]), 200, headers)
 
 
-class Logout(MethodView):
-    @login_required
-    def get(self):
-        logout_user()
-        return Response('<p>Logged out</p>')
 
-class Admin(MethodView):
-    @login_required
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('admin.html'), 200, headers)
+
+
+        return make_response('success', '200', {'Content-Type': 'text'})
