3838c15f0a17a64e9ccdaf054ecfe5cf2ac94135
Zi <zi.s.gao@gmail.com>
2018-01-28 09:41:55 -0500

    resolved conflicts and merged Anthony's stuff

--- web/static/camera.js
+++ web/static/camera.js
@@ -339,3 +339,11 @@ function return_to_pic(){
     video.play();
 }
 
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
